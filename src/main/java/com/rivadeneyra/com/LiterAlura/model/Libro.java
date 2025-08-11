d7e6eabe0e0c5f8dd60b91fcf0e73c73cc1ff026
Jonas Rivadeneyra <aznaranjonas@gmail.com>
2025-08-11 01:44:22 -0500

    3

--- src/main/java/com/rivadeneyra/com/LiterAlura/model/Libro.java
+++ src/main/java/com/rivadeneyra/com/LiterAlura/model/Libro.java
@@ -1,4 +1,31 @@
 package com.rivadeneyra.com.LiterAlura.model;
 
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "libros")
 public class Libro {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String titulo;
+    @ManyToOne
+    private Autor autor;
+    @Enumerated(EnumType.STRING)
+    private Idioma lenguaje;
+    private Integer numero_descargas;
+
+    public Libro(){}
+
+    public Libro(DatosLibros datosLibro){
+        this.titulo = datosLibro.titulo();
+        this.lenguaje = Idioma.fromString(datosLibro.idiomas().toString().split(",")[0].trim());
+        this.numero_descargas = datosLibro.numero_descargas();
+    }
+
+
+
+
+
